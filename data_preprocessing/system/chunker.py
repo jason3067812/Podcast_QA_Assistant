b868cdb757e827ae961b8144f099ccf061bbb429
Rerowine <144857205+Rerowine@users.noreply.github.com>
2023-12-05 01:27:55 -0500

    Add files via upload

--- data_preprocessing/system/chunker.py
+++ data_preprocessing/system/chunker.py
@@ -2,6 +2,7 @@ import nltk
 import spacy
 import os
 import sys
+import pickle
 
 nltk.download('punkt')
 spacy.prefer_gpu()
@@ -49,10 +50,39 @@ def chunk_folder(dir_path, output_path, method=SPACY, chunk_size=20):
             file_path = os.path.join(root, filename)
             chunk_file(file_path, output_path, method, chunk_size)
 
+
+
+def embed_file(file_path, output_path):
+    file_name_without_extension, file_extension = os.path.splitext(os.path.basename(file_path))
+    nlp = spacy.load('en_core_web_lg')
+
+    with open(file_path, 'r') as file:
+        text = file.read()
+
+    doc = nlp(text)
+    embedding = doc.vector
+
+    filename =  f"{file_name_without_extension}_embedded_.txt"
+    output_filename = output_path + "/" + filename
+
+    with open(output_filename, 'wb') as out_file:
+        pickle.dump(embedding, out_file)
+
+
+def embed_folder(dir_path, output_path):
+     for root, dirs, files in os.walk(dir_path):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            embed_file(file_path, output_path)
+
+
+
 if __name__ == '__main__':
+    
     chunk_size, method, dir_path, output_path, to_chunk  = sys.argv[1:]
     # chunk_size, method, dir_path, output_path, to_chunk = 20, SPACY, './input', './chunked_out', FOLDER
     if to_chunk == FOLDER:
         chunk_folder(dir_path, output_path, method, chunk_size)
     else:
         chunk_file(dir_path, output_path, method, chunk_size)
+    
