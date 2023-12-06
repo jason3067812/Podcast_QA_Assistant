15bfc6e0bd008097aa72c201dbd74896b4ecba23
Eric Chang <jason3067812@gmail.com>
2023-12-05 22:37:48 -0500

    update gpt engine

--- data_preprocessing/chunker.py
+++ data_preprocessing/chunker.py
@@ -13,11 +13,17 @@ NLTK = 'nltk'
 FOLDER = 'folder'
 FILE = 'file'
 
+
 def chunker_helper(sentences, chunk_size, overlap_size):
-    pass
+    chunks = []
+    for i in range(0, len(sentences), chunk_size - overlap_size):
+        chunk = sentences[i:i + chunk_size]
+        chunks.append(chunk)
+    return chunks
 
 
 def chunker(data, chunk_size, overlap_size, method):
+    
     if method == SPACY:
         nlp = spacy.load(SPACY_MODEL)
         doc = nlp(data)
@@ -26,37 +32,41 @@ def chunker(data, chunk_size, overlap_size, method):
             sentences.append(sentence.text)
     elif method == NLTK:
         sentences = nltk.sent_tokenize(data)
+        
     chunks = chunker_helper(sentences, chunk_size, overlap_size)
-    # chunks = ['\n'.join(sentences[i:i + chunk_size]) for i in range(0, len(sentences), chunk_size)]
     
     return chunks, len(chunks)
 
+
 def _save_chunked_file(chunk, filename, output_path, i):
+    
     output_filename = output_path + "/" + filename
     with open(output_filename, 'w', encoding='utf-8') as file:
         file.write(chunk)
     print(f"Chunk {i + 1} save into {filename}")   
 
-def chunk_file(file_path, output_path, method=SPACY, chunk_size=20):
+
+def chunk_file(file_path, output_path, method=SPACY, chunk_size = 100, overlap_size = 50):
+    
     file_name_without_extension, file_extension = os.path.splitext(os.path.basename(file_path))
     with open(file_path, 'r') as file:
         content = file.read()
-    chunks, chuncks_length = chunker(content, chunk_size, method)
+    chunks, chuncks_length = chunker(content, chunk_size, method, overlap_size)
     for i, chunk in enumerate(chunks):
         filename = f"{file_name_without_extension}_chunk_{i + 1}.txt"
         _save_chunked_file(chunk, filename, output_path, i)
 
 
-
-def chunk_folder(dir_path, output_path, method=SPACY, chunk_size=20):
+def chunk_folder(dir_path, output_path, method=SPACY, chunk_size = 100, overlap_size = 50):
+    
     for root, dirs, files in os.walk(dir_path):
         for filename in files:
             file_path = os.path.join(root, filename)
-            chunk_file(file_path, output_path, method, chunk_size)
-
+            chunk_file(file_path, output_path, method, chunk_size, overlap_size)
 
 
 def embed_file(file_path, output_path):
+    
     file_name_without_extension, file_extension = os.path.splitext(os.path.basename(file_path))
     nlp = spacy.load('en_core_web_lg')
 
@@ -74,6 +84,7 @@ def embed_file(file_path, output_path):
 
 
 def embed_folder(dir_path, output_path):
+    
      for root, dirs, files in os.walk(dir_path):
         for filename in files:
             file_path = os.path.join(root, filename)
@@ -82,9 +93,10 @@ def embed_folder(dir_path, output_path):
 
 
 if __name__ == '__main__':
-    chunk_size, method, dir_path, output_path, to_chunk  = sys.argv[1:]
+    
+    overlap_size, chunk_size, method, dir_path, output_path, to_chunk  = sys.argv[1:]
     # chunk_size, method, dir_path, output_path, to_chunk = 20, SPACY, './input', './chunked_out', FOLDER
     if to_chunk == FOLDER:
-        chunk_folder(dir_path, output_path, method, chunk_size)
+        chunk_folder(dir_path, output_path, method, chunk_size, overlap_size)
     else:
-        chunk_file(dir_path, output_path, method, chunk_size)    
+        chunk_file(dir_path, output_path, method, chunk_size, overlap_size)    
--- system.py
+++ system.py
@@ -1,1 +1,49 @@
-syste
+from openai import OpenAI
+
+
+
+def pass_to_llm(title, chunk_info, query, client, model_id):
+    
+    print("input prompt: ")
+    
+    prompt = f"According to the following information: {chunk_info}\nAnswer the following question: {query}"
+
+    print(prompt)
+
+
+    messages = [
+        
+            {"role": "user", "content": prompt}
+
+        ]
+    
+    
+    completion = client.chat.completions.create(
+    model=model_id,
+    messages=messages,
+    )
+    
+    response = completion.choices[0].message.content
+    
+    
+    return response
+    
+    
+
+def main():
+    
+    
+    title = ""
+    chunk_info = ""
+    query = ""
+    
+    
+    api_key = ""
+    client = OpenAI(api_key=api_key)
+    model_id = "gpt-3.5-turbo-1106"
+    
+    response = pass_to_llm(title, chunk_info, query, client, model_id)
+    
+
+    
+    
